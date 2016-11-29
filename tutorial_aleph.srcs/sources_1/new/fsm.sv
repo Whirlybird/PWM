03db0bb109012a1683ae9510b2cf9ce8126905ad
Spencer <spencerd56@gmail.com>
2016-11-29 08:16:29 -0800

    Split PWM into 4 motors

--- tutorial_aleph.srcs/sources_1/new/fsm.sv
+++ tutorial_aleph.srcs/sources_1/new/fsm.sv
@@ -19,25 +19,94 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module pwm_select(
-    input [1:0] switches,
+    input [3:0] setPWM1,
+    input [3:0] setPWM2,
+    input [3:0] setPWM3,
+    input [3:0] setPWM4,
     output [29:0] count,
-    output [29:0] limit
+    output [29:0] motor1,
+    output [29:0] motor2,
+    output [29:0] motor3,
+    output [29:0] motor4
     );
-
-    logic [29:0] s_limit;
-
     assign count = 'd249_999;
-    assign limit = s_limit;
+    
+    logic [29:0] s_motor1;
+    logic [29:0] s_motor2;
+    logic [29:0] s_motor3;
+    logic [29:0] s_motor4;
+    
+    assign motor1 = s_motor1;
     always_comb begin
-        case (switches)
-            'b00 : s_limit = 'd74_999;
-            'b01 : s_limit = 'd174_999;
-            'b10 : s_limit = 'd124_999;
-            'b11 : s_limit = 'd199_999;
-            default: s_limit = 'd124_999;
+        case (setPWM1)
+            'h0 : s_motor1 = 'd74_999;
+            'h1 : s_motor1 = 'd84_999;
+            'h2 : s_motor1 = 'd94_999;
+            'h3 : s_motor1 = 'd104_999;
+            'h4 : s_motor1 = 'd114_999;
+            'h5 : s_motor1 = 'd124_999;
+            'h6 : s_motor1 = 'd134_999;
+            'h7 : s_motor1 = 'd144_999;
+            'h8 : s_motor1 = 'd154_999;
+            'h9 : s_motor1 = 'd164_999;
+           'h10 : s_motor1 = 'd179_999;
+            default: s_motor1 = 'd124_999;
         endcase
     end
 
+    assign motor2 = s_motor2;
+    always_comb begin
+        case (setPWM2)
+            'h0 : s_motor2 = 'd74_999;
+            'h1 : s_motor2 = 'd84_999;
+            'h2 : s_motor2 = 'd94_999;
+            'h3 : s_motor2 = 'd104_999;
+            'h4 : s_motor2 = 'd114_999;
+            'h5 : s_motor2 = 'd124_999;
+            'h6 : s_motor2 = 'd134_999;
+            'h7 : s_motor2 = 'd144_999;
+            'h8 : s_motor2 = 'd154_999;
+            'h9 : s_motor2 = 'd164_999;
+           'h10 : s_motor2 = 'd179_999;
+            default: s_motor2 = 'd124_999;
+        endcase
+    end
+    
+    assign motor3 = s_motor3;
+    always_comb begin
+        case (setPWM3)
+            'h0 : s_motor3 = 'd74_999;
+            'h1 : s_motor3 = 'd84_999;
+            'h2 : s_motor3 = 'd94_999;
+            'h3 : s_motor3 = 'd104_999;
+            'h4 : s_motor3 = 'd114_999;
+            'h5 : s_motor3 = 'd124_999;
+            'h6 : s_motor3 = 'd134_999;
+            'h7 : s_motor3 = 'd144_999;
+            'h8 : s_motor3 = 'd154_999;
+            'h9 : s_motor3 = 'd164_999;
+           'h10 : s_motor3 = 'd179_999;
+            default: s_motor3 = 'd124_999;
+        endcase
+    end
+        
+    assign motor4 = s_motor4;
+    always_comb begin
+        case (setPWM4)
+            'h0 : s_motor4 = 'd74_999;
+            'h1 : s_motor4 = 'd84_999;
+            'h2 : s_motor4 = 'd94_999;
+            'h3 : s_motor4 = 'd104_999;
+            'h4 : s_motor4 = 'd114_999;
+            'h5 : s_motor4 = 'd124_999;
+            'h6 : s_motor4 = 'd134_999;
+            'h7 : s_motor4 = 'd144_999;
+            'h8 : s_motor4 = 'd154_999;
+            'h9 : s_motor4 = 'd164_999;
+           'h10 : s_motor4 = 'd179_999;
+            default: s_motor4 = 'd124_999;
+        endcase
+    end
 endmodule
+
